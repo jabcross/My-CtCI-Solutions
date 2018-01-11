66a804c1a1bb27f54f729a97cc1f1d56a2347f8e
Pedro Ciambra <pedrociambra@gmail.com>
2018-01-11 16:03:40 -0200

    1.2 completed

---
+++ 1.2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Uses array of ints to count number of each character in each string.
+bool is_permutation(string a, string b){
+    if (a.size() != b.size()) return false;
+    int chars_a[256], chars_b[256];
+    for (int i = 0; i < 256; i++) chars_a[i] = chars_b[i] = 0;
+    for (auto chr: a){
+        chars_a[chr] += 1;
+    }
+    for (auto chr: b){
+        chars_b[chr] += 1;
+    }
+    for (auto chr: b){
+        if (chars_a[chr] != chars_b[chr]) return false;
+    }
+    return true;
+}
+
+int main(){
+    assert(is_permutation("","") == true);
+    assert(is_permutation("avocado","adocavo") == true);
+    assert(is_permutation("avocado","banana") == false);
+    assert(is_permutation("avocado","abanana") == false);
+    cout << "Tests passed." << endl;
+    return 0;
+}
