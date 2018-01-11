a89a156fb7a74c26339ba9a13abcce6cf62c371e
Pedro Ciambra <pedrociambra@gmail.com>
2018-01-11 15:53:22 -0200

    1.1 completed

---
+++ 1.1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Uses array of bools to check if the character has been seen before.
+bool is_unique(string str){
+    bitset<256> chars;
+    for (auto chr: str){
+        if (chars[chr]){
+            return false;
+        }
+        chars[chr] = true;
+    }
+    return true;
+}
+
+int main(){
+    assert(is_unique("") == true);
+    assert(is_unique("a") == true); 
+    assert(is_unique("abcde") == true);
+    assert(is_unique("abcba") == false);
+    cout << "Tests passed." << endl;
+    return 0;
+}
